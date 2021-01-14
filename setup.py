ad83fcceced79371aa399ff840d0499f8c2f24ec
nhatsmrt <nphamcs@gmail.com>
2021-01-13 22:44:08 -0800

    Update setup script

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 with open('requirements.txt', 'r') as req_file:
     requirements = [line[:-1] for line in req_file if len(line) > 1]
@@ -10,6 +10,7 @@ setup(
     author_email="nphamcs@gmail.com",
     description="A small tool for retrieving relevant passages to a question from Wikipedia",
     py_modules=['wiki_passage_retriever'],
+    packages=find_packages(),
     install_requires=requirements,
     url="https://github.com/nhatsmrt/wiki-dpr",
     entry_points={
