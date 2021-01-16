51009d309931b9a2ffae7984f759c7ef73d125e3
nhatsmrt <nphamcs@gmail.com>
2021-01-16 11:46:47 -0800

    Implements index functionality for flask application

--- flask-app/api.py
+++ flask-app/api.py
@@ -1,11 +1,19 @@
 from flask import Flask, request, render_template, jsonify
 from wiki_passage_retriever.retrieve import get_most_relevant_passages
+from wiki_passage_retriever.index import index_wikipedia
+import uuid
+import json
+import sqlite3
 
+
+DATABASE_NAME = "wikipedia_index.db"
 app = Flask(__name__)
+
 
 @app.route('/')
 def home():
     return render_template('home.html')
+
 
 @app.route('/retrieve', methods=['GET'])
 def retrieve():
@@ -13,8 +21,27 @@ def retrieve():
     query = request.args.get('query')
     topk = int(request.args.get("topk"))
 
+    # TODO: check if the query has already been indexed:
+
     print(question, query, topk)  # for debugging
     return jsonify(result=get_most_relevant_passages(query, question, topk))
+
+
+@app.route('/index', methods=['POST'])
+def index():
+    query = request.args.get('query')
+    index_dir_path = str(uuid.uuid4())  # randomize a name to store queries
+    index_wikipedia(query, index_dir_path)
+
+    # Inserts metadata to database:
+    with sqlite3.connect(DATABASE_NAME) as conn:
+        cursor = conn.cursor()
+        sql_query = "INSERT INTO wikipedia_index(query_name, index_dir_path) VALUES(?, ?);"
+        cursor.execute(sql_query, [query, index_dir_path])
+        conn.commit()
+
+    return json.dumps({'success': True}), 200, {'ContentType': 'application/json'}
+
 
 if __name__ == '__main__':
     app.run(debug=False)
--- flask-app/schema.sql
+++ flask-app/schema.sql
@@ -3,6 +3,6 @@ DROP TABLE IF EXISTS wikipedia_index;
 
 CREATE TABLE wikipedia_index(
     id INTEGER PRIMARY KEY AUTOINCREMENT,
-    query_name TEXT UNIQUE NOT NULL,
+    query TEXT UNIQUE NOT NULL,
     index_dir_path TEXT UNIQUE NOT NULL
 );
