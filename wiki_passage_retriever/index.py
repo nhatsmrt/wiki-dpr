c18e3e5a5f76972b7c931430cfc5ef3db00a5ba9
Nhat Pham <nphamcs@gmail.com>
2021-01-21 15:54:08 -0800

    Adds documentation (#12)
    * Add documentation for indexing methods
    
    * Write doc for some retrieve functions
    
    * Add doc to span methods
    
    * Add documentation for utils methods
    
    * Remove a redundant line
    
    * Add doc to one method

--- wiki_passage_retriever/index.py
+++ wiki_passage_retriever/index.py
@@ -12,6 +12,12 @@ EMBEDDING_DIMENSION = 768
 
 
 def encode_passages(passages: List[str]) -> ndarray:
+    """
+    Computes the embedding of the given passages.
+
+    :param passages: passges to be encoded.
+    :return: a matrix, where each row is the embedding of a passage
+    """
     with torch.no_grad():
         tokenizer = DPRContextEncoderTokenizer.from_pretrained('facebook/dpr-ctx_encoder-single-nq-base')
         model = DPRContextEncoder.from_pretrained('facebook/dpr-ctx_encoder-single-nq-base')
@@ -20,6 +26,12 @@ def encode_passages(passages: List[str]) -> ndarray:
 
 
 def encode_question(question: str) -> ndarray:
+    """
+    Computes the embedding of the given question
+
+    :param question: question to be encoded.
+    :return: a 1D numpy array, representing the embedding of the question.
+    """
     with torch.no_grad():
         tokenizer = DPRQuestionEncoderTokenizer.from_pretrained('facebook/dpr-question_encoder-single-nq-base')
         model = DPRQuestionEncoder.from_pretrained('facebook/dpr-question_encoder-single-nq-base')
@@ -28,6 +40,12 @@ def encode_question(question: str) -> ndarray:
 
 
 def annoy_index_passages(passages: List[str], index_dir_path: str):
+    """
+    Indexes the given passages via `annoy`, using the dot product similarity function, and storing the index (along with the texts) at the given path.
+
+    :param passages: passages to be indexed.
+    :param index_dir_path: path to store the texts and the index.
+    """
     # Create index directiory, if not exist:
     if not os.path.exists(index_dir_path):
         os.makedirs(index_dir_path)
@@ -48,10 +66,24 @@ def annoy_index_passages(passages: List[str], index_dir_path: str):
 
 
 def index_wikipedia(query: str, index_dir_path: str):
+    """
+    Indexes and saves an entire Wikipedia page.
+
+    :param query: search query for Wikipedia.
+    :param index_dir_path:
+    """
     annoy_index_passages(retrieve_wiki_page(query), index_dir_path)
 
 
-def retrieve_by_index(index_dir_path: str, question: str, top_k: int=1):
+def retrieve_by_index(index_dir_path: str, question: str, top_k: int=1) -> List[str]:
+    """
+    Retrieves the most relevant passages to a question, using the indexed embedding of the passages.
+
+    :param index_dir_path: path to the index and texts.
+    :param question:
+    :param top_k: number of relevant passages to retrieve.
+    :return: list of the most relevant passages.
+    """
     question_embedding = encode_question(question)
     indexer = AnnoyIndex(EMBEDDING_DIMENSION, 'dot')
     indexer.load("{}/index.ann".format(index_dir_path))
--- wiki_passage_retriever/retrieve.py
+++ wiki_passage_retriever/retrieve.py
@@ -11,6 +11,14 @@ __all__ = ['get_most_relevant_spans_from_wiki', 'get_most_relevant_passages']
 
 
 def process_with_dpr_reader(passages: List[str], titles: Union[List[str], str], question: str) -> Tuple[MyDPRReaderTokenizer, Dict[str, List[List[int]]], DPRReaderOutput]:
+    """
+    Processes passages (along with the titles and question) using a dense passage retrieval (DPR) model.
+
+    :param passages:
+    :param titles:
+    :param question:
+    :return: The tokenizer, encoded inputs, and the outputs of the DPR model.
+    """
     if isinstance(titles, str):
         return process_with_dpr_reader(passages, [titles] * len(passages), question)
 
@@ -30,23 +38,52 @@ def process_with_dpr_reader(passages: List[str], titles: Union[List[str], str], 
 
 def get_relevance_scores(passages: List[str], titles: Union[List[str], str], question: str) -> ndarray:
     """
-    Given a list of passages, a list of corresponding titles (or a single title if all passages are in the same article), and a question,
-    returns the relevance score of the passages with respect to the question.
+    Computes the relevance score of the passages with respect to a question.
+
+    :param passages:
+    :param titles: a list of corresponding titles (or a single title if all passages are in the same article),
+    :param question:
+    :return: the relevance score of the passages with respect to the question.
     """
     _, _, outputs = process_with_dpr_reader(passages, titles, question)
     return outputs.relevance_logits.numpy()
 
 
 def get_most_relevant_spans(passages: List[str], titles: Union[List[str], str], question: str, top_k: int=1) -> List[str]:
+    """
+    Gets the most relevant spans from the list of passages
+
+    :param passages:
+    :param titles:
+    :param question:
+    :param top_k: number of relevant spans to retrieve.
+    :return: list of the most relevant spans.
+    """
     tokenizer, encoded_inputs, outputs = process_with_dpr_reader(passages, titles, question)
     return [span.text for span in tokenizer.decode_best_spans(encoded_inputs, outputs, num_spans=top_k)]
 
 
 def get_most_relevant_spans_from_wiki(search_query: str, question: str, top_k: int=1) -> List[str]:
+    """
+    Gets the most relevant spans to a question from all the passages of a Wikipedia page.
+
+    :param search_query: the search query for Wikipedia. Will return the passages from the first result.
+    :param question:
+    :param top_k: number of relevant spans to retrieve.
+    :return: list of the most relevant spans.
+    """
     return get_most_relevant_spans(retrieve_wiki_page(search_query), search_query, question, top_k)
 
 
 def get_most_relevant_passages(search_query: str, question: str, top_k: int=1) -> List[str]:
+    """
+    Retrieves the most relevant passages to a question from a Wikipedia page
+
+    :param search_query: the search query for Wikipedia. Will return the passages from the first result.
+    :param question:
+    :param top_k: number of relevant passages to retrieve.
+    :return: list of the most relevant passages.
+    """
     passages = retrieve_wiki_page(search_query)
     relevance_scores = get_relevance_scores(passages, search_query, question)
 
